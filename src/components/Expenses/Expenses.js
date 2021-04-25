bd013beac10222ca4668cd9c85221217fd0ae264
AndrePreto <a.preto@campus.fct.unl.pt>
2021-04-25 19:05:45 +0100

    feature/learning-to-use-state - Using button to change ExpenseItem title through useState Hook

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,22 +1,20 @@
-import React from 'react';
+import React from "react";
 
 import ExpenseItem from "./ExpenseItem";
-import Card from '../UI/Card'
+import Card from "../UI/Card";
 import "./Expenses.css";
 
 const Expenses = (props) => {
   const items = props.items;
-  return (
-    <Card className="expenses">
-      {items.map((expense) => (
-        <ExpenseItem
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
-    </Card>
-  );
-}
+  const expenseItems = items.map((expense) => (
+    <ExpenseItem
+      key={expense.id}
+      title={expense.title}
+      amount={expense.amount}
+      date={expense.date}
+    />
+  ));
+  return <Card className="expenses">{expenseItems}</Card>;
+};
 
 export default Expenses;
